10060c31ea0353efa8d0cbd90354c17231cacf55
chinoy <chinoy316@gmail.com>
2025-01-29 22:24:47 +0800

    Moved data to an external file for cleanliness.

---
+++ assets/foldersData.js
@@ -0,0 +1,98 @@
+// src/assets/foldersData.js
+export const foldersData = [
+	{
+		name: 'Grade 9',
+		pdfs: [],
+		subfolders: [
+			{
+				name: 'Quarter 1',
+				pdfs: [
+					{
+						title: 'Module 1',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_1/HE_DRESSMAKING_GR9_Q1_MODULE-1.pdf',
+					},
+					{
+						title: 'Module 2',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_1/HE_DRESSMAKING_GR9_Q1_MODULE-2.pdf',
+					},
+					{
+						title: 'Module 3',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_1/HE_DRESSMAKING_GR9_Q1_MODULE-3.pdf',
+					},
+					{
+						title: 'Module 4',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_1/HE_DRESSMAKING_GR9_Q1_MODULE-4.pdf',
+					},
+					{
+						title: 'Module 5',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_1/HE_DRESSMAKING_GR9_Q1_MODULE-5.pdf',
+					},
+				],
+				subfolders: [],
+			},
+			{
+				name: 'Quarter 2',
+				pdfs: [
+					{
+						title: 'Module 1',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_2/DRESSMAKING 9_Q2_MODULE-1.pdf',
+					},
+					{
+						title: 'Module 2',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_2/DRESSMAKING 9_Q2_MODULE-2.pdf',
+					},
+					{
+						title: 'Module 3',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_2/DRESSMAKING 9_Q2_MODULE-3.pdf',
+					},
+					{
+						title: 'Module 4',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_2/DRESSMAKING 9_Q2_MODULE-4.pdf',
+					},
+					{
+						title: 'Module 5',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_2/DRESSMAKING 9_Q2_MODULE-5.pdf',
+					},
+					{
+						title: 'Module 6',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_2/DRESSMAKING 9_Q2_MODULE-6.pdf',
+					},
+					{
+						title: 'Module 7',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_2/DRESSMAKING 9_Q2_MODULE-7.pdf',
+					},
+					// more modules...
+				],
+				subfolders: [],
+			},
+		],
+	},
+	{
+		name: 'Grade 10',
+		pdfs: [
+			{
+				title: 'Polyester',
+				description: 'A guide to working with polyester fabric.',
+				url: '/pdf/Polyester.pdf',
+			},
+			{
+				title: 'Nylon',
+				description: 'A guide to working with nylon fabric.',
+				url: '/pdf/Nylon.pdf',
+			},
+		],
+		subfolders: [],
+	},
+]
