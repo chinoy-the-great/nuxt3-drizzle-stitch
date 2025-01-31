3c2b6ba49b984e5d3db13aeb17bcaa93509e244b
chinoy <chinoy316@gmail.com>
2025-01-31 21:49:14 +0800

    Updated styling for sidebar.

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,12 +1,12 @@
 <template>
 	<aside
-		class="fixed inset-y-0 left-0 z-30 w-64 bg-white shadow-lg transition-transform transform"
+		class="fixed inset-y-0 left-0 z-30 w-64 bg-nude-700 bg-opacity-90 shadow-lg transition-transform transform"
 		:class="{ '-translate-x-full': !isOpen, 'translate-x-0': isOpen }"
 	>
 		<div class="p-4 relative">
 			<!-- Close Button -->
 			<button
-				class="absolute top-4 right-4 text-nude-600 hover:text-nude-800 focus:outline-none"
+				class="absolute top-4 right-4 text-white hover:text-nude-800 focus:outline-none"
 				@click="emit('close')"
 			>
 				<svg
@@ -23,39 +23,47 @@
 
 			<!-- Avatar and Name Section -->
 			<div class="flex flex-col items-center mb-6">
-				<img
-					src="/Your_Account_Icon.png"
-					alt="Avatar"
-					class="32 h-32 rounded-full mb-2 border border-nude-300"
-				/>
-				<h3 class="text-nude-700 text-lg font-semibold">Your Account</h3>
+				<img src="/Your_Account_Icon.png" alt="Avatar" class="32 h-32 mb-2 filter invert brightness-200" />
+				<h3 class="text-white text-lg font-semibold">Name</h3>
 			</div>
 
 			<!-- Sidebar content -->
-			<h2 class="text-nude-700 text-xl font-bold mb-4">Navigation</h2>
+			<h2 class="text-white text-center text-xl font-bold mb-1 border-b border-t pt-1 pb-1">YOUR ACCOUNT</h2>
 			<ul class="space-y-2">
-				<li>
-					<NuxtLink to="/settings" class="flex items-center text-nude-700 hover:text-nude-600">
+				<li class="border-b border-white pb-1">
+					<NuxtLink to="/settings" class="flex items-center text-white hover:text-nude-600">
 						<!-- Image Icon -->
-						<img src="/public/Settings_Icon.png" alt="Settings" class="w-5 h-5 mr-3" />
+						<img
+							src="/public/Settings_Icon.png"
+							alt="Settings"
+							class="w-5 h-5 mr-3 filter invert brightness-200"
+						/>
 						Settings
 					</NuxtLink>
 				</li>
-				<li>
-					<NuxtLink to="/references" class="flex items-center text-nude-700 hover:text-nude-600">
+				<li class="border-b border-white pb-1">
+					<NuxtLink to="/references" class="flex items-center text-white hover:text-nude-600">
 						<!-- Image Icon -->
-						<img src="/public/References_Icon.png" alt="References" class="w-5 h-5 mr-3" />
+						<img
+							src="/public/References_Icon.png"
+							alt="References"
+							class="w-5 h-5 mr-3 filter invert brightness-200"
+						/>
 						References
 					</NuxtLink>
 				</li>
-				<li>
+				<li class="border-b border-white pb-1">
 					<NuxtLink
 						to="/login"
-						class="flex items-center text-nude-700 hover:text-nude-600"
+						class="flex items-center text-white hover:text-nude-600"
 						@click="userStore.clearUser"
 					>
 						<!-- Image Icon -->
-						<img src="/public/Log_out_Icon.png" alt="Logout" class="w-5 h-5 mr-3" />
+						<img
+							src="/public/Log_out_Icon.png"
+							alt="Logout"
+							class="w-5 h-5 mr-3 filter invert brightness-200"
+						/>
 						Logout
 					</NuxtLink>
 				</li>
