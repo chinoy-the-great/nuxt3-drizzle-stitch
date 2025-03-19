2d1e31565c83281db5f513c9ca1cce221be45d13
chinoy <chinoy316@gmail.com>
2025-03-20 05:46:46 +0800

    Updated Folders Data to include all Fabric Guide modules.

--- assets/foldersData.js
+++ assets/foldersData.js
@@ -77,22 +77,178 @@ export const foldersData = [
 				],
 				subfolders: [],
 			},
+			{
+				name: 'Quarter 3',
+				pdfs: [
+					{
+						title: 'Module 1',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_3/dressmaking-9-module-1.pdf',
+					},
+					{
+						title: 'Module 2',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_3/dressmaking-9-module-2.pdf',
+					},
+				],
+				subfolders: [],
+			},
+			{
+				name: 'Quarter 4',
+				pdfs: [
+					{
+						title: 'Module 1',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_4/Quarter-4-Dressmaking-9-MODULE-1.pdf',
+					},
+					{
+						title: 'Module 2',
+						description: '',
+						url: '/pdf/Grade_9/Quarter_4/Quarter-4-Dressmaking-9-MODULE-2.pdf',
+					},
+				],
+				subfolders: [],
+			},
 		],
 	},
 	{
 		name: 'Grade 10',
-		pdfs: [
+		pdfs: [],
+		subfolders: [
 			{
-				title: 'Polyester',
-				description: 'A guide to working with polyester fabric.',
-				url: '/pdf/Polyester.pdf',
+				name: 'Quarter 1',
+				pdfs: [
+					{
+						title: 'Module 1',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_1/HE_DRESSMAKING_GR10_Q1_MODULE 1 for teacher.pdf',
+					},
+					{
+						title: 'Module 2',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_1/HE_DRESSMAKING_GR10_Q1_MODULE 2 for student.pdf',
+					},
+					{
+						title: 'Module 3',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_1/HE_DRESSMAKING_GR10_Q1_MODULE 3 for student.pdf',
+					},
+					{
+						title: 'Module 4',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_1/HE_DRESSMAKING_GR10_Q1_MODULE 4 for student.pdf',
+					},
+					{
+						title: 'Module 5',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_1/HE_DRESSMAKING_GR10_Q1_MODULE 5 for student.pdf',
+					},
+					{
+						title: 'Module 6',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_1/HE_DRESSMAKING_GR10_Q1_MODULE 6 for student.pdf',
+					},
+					{
+						title: 'Module 7',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_1/HE_DRESSMAKING_GR10_Q1_MODULE 7 for teacher.pdf',
+					},
+				],
+				subfolders: [],
 			},
 			{
-				title: 'Nylon',
-				description: 'A guide to working with nylon fabric.',
-				url: '/pdf/Nylon.pdf',
+				name: 'Quarter 2',
+				pdfs: [
+					{
+						title: 'Module 1',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_2/HE_DRESSMAKING_GR10_Q2_MODULE1.pdf',
+					},
+					{
+						title: 'Module 2',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_2/HE_DRESSMAKING_GR10_Q2_MODULE2.pdf',
+					},
+					{
+						title: 'Module 4',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_2/HE_DRESSMAKING_GR10_Q2_MODULE4.pdf',
+					},
+				],
+				subfolders: [],
+			},
+			{
+				name: 'Quarter 3',
+				pdfs: [
+					{
+						title: 'Module 1',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_3/dressmaking-10-module-1.pdf',
+					},
+					{
+						title: 'Module 2',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_3/dressmaking-10-module-2.pdf',
+					},
+					{
+						title: 'Module 3',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_3/dressmaking-10-module-3.pdf',
+					},
+					{
+						title: 'Module 4',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_3/dressmaking-10-module-4.pdf',
+					},
+					{
+						title: 'Module 5',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_3/dressmaking-10-module-5.pdf',
+					},
+					{
+						title: 'Module 6',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_3/dressmaking-10-module-6.pdf',
+					},
+					{
+						title: 'Module 7',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_3/dressmaking-10-module-7.pdf',
+					},
+				],
+				subfolders: [],
+			},
+			{
+				name: 'Quarter 4',
+				pdfs: [
+					{
+						title: 'Module 1',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_4/HE-DRESSMAKING10-Quarter4-Module-1.pdf',
+					},
+					{
+						title: 'Module 2',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_4/HE-DRESSMAKING10-Quarter4-Module-2.pdf',
+					},
+					{
+						title: 'Module 3',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_4/HE-DRESSMAKING10-Quarter4-Module-3.pdf',
+					},
+					{
+						title: 'Module 4',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_4/HE-DRESSMAKING10-Quarter4-Module-4.pdf',
+					},
+					{
+						title: 'Module 5',
+						description: '',
+						url: '/pdf/Grade_10/Quarter_4/HE-DRESSMAKING10-Quarter4-Module-5.pdf',
+					},
+				],
+				subfolders: [],
 			},
 		],
-		subfolders: [],
 	},
 ]
