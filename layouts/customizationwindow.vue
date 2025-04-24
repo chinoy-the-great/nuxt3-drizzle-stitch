339022f075ebd955c4a78aa897ba3e613338045e
chinoy <chinoy316@gmail.com>
2025-04-24 19:21:25 +0800

    Added custom settings for Customization Window layout type.

--- layouts/customizationwindow.vue
+++ layouts/customizationwindow.vue
@@ -1,7 +1,13 @@
 <template>
 	<div :class="layoutClasses" class="flex flex-col min-h-screen bg-white">
 		<!-- Navbar -->
-		<header class="bg-[#ffe9fa] w-full z-30 border-b-2 border-[#ffa5a5]">
+		<header
+			class="w-full z-30 border-b-2"
+			:style="{
+				backgroundColor: headerBgColor,
+				borderColor: headerBorderColor,
+			}"
+		>
 			<nav class="container mx-auto px-4 py-4 flex justify-between items-center">
 				<!-- Left: Back Icon and Page Title -->
 				<div class="flex items-center space-x-8">
@@ -14,18 +20,30 @@
 		</header>
 
 		<!-- Page content -->
-		<main class="flex-grow container mx-auto bg-[#ffe9fa]">
+		<main class="flex-grow container mx-auto" :style="{ backgroundColor: mainBgColor }">
 			<NuxtPage />
 		</main>
 	</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 
 const pageTitle = computed(() => route.meta.title || '')
+
+const headerBgColor = computed<string>(() => {
+	return (route.meta.headerBg as string) ?? '#ffe9fa'
+})
+
+const headerBorderColor = computed<string>(() => {
+	return (route.meta.headerBorder as string) ?? '#ffa5a5'
+})
+
+const mainBgColor = computed<string>(() => {
+	return (route.meta.mainBg as string) ?? '#ffe9fa'
+})
 
 // Handle Back Button Click
 const onBackClick = () => {
